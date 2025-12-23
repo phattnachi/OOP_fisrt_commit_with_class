a465b379c1d58a51563a1b8f0c65ccf92870c353
phattnachi <phattnachi@gmail.com>
2025-12-23 14:26:23 +0700

    crate class EV

--- main.py
+++ main.py
@@ -8,3 +8,15 @@ class car:
                
 my_car = car('Toyota','Honda')
 my_car.drive()
+
+
+class ElectricCar(car):
+    def __init__(self, brand, model,battery_size):
+        super().__init__(model,brand)
+        self.battery_size = battery_size
+
+    def charge(self):
+        print(f'{self.brand} ,{self.model}, battery is charging....{self.battery_size}')
+
+car = ElectricCar('Toyota','Honda',150)
+car.charge()
